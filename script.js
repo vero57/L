ae9b896431e852c05794dea99be75577ee00e996
vero57 <yosuagerrad@gmail.com>
2024-10-31 21:49:59 +0700

    pengen buat aja

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("idk about this, but please click this link")
+console.log("https://open.spotify.com/playlist/3Oyd64v9VAfvch2gpDOF8j?si=2b66ac3410484cd6")
+
+let L = 'I love you'
+
+for (Y = 0; Y <167;Y++) {
+    console.log(L)
+}
